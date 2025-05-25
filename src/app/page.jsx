4e5dc11c4c177290b7609e9db81de8bbd0231f5a
Md Roknujjaman Rony <rony@Mds-MacBook-Pro.local>
2025-05-25 14:59:58 +0600

    added particles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,8 @@ import Contact from "./components/Contact";
 import Education from "./components/Education";
 
 import Footer from "./components/Footer";
+import ParticlesBg from "./components/Particles/Particles";
+
 import Projects from "./components/Projects";
 import SkillsSection from "./components/SkillsSection";
 import { FaArrowCircleUp } from "react-icons/fa";
@@ -14,18 +16,24 @@ export default function Home() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
-    <section className="w-11/12 mx-auto ">
-      <Banner />
-      <AboutMe></AboutMe>
-      <SkillsSection />
-      <Education />
-      <Projects />
-      <Contact />
-      <Footer />
-      <FaArrowCircleUp
-        onClick={bottomToTop}
-        className="fixed bottom-4 right-3 size-10 z-50 cursor-pointer"
-      />
-    </section>
+    <>
+      <div className="fixed top-0 left-0 w-full h-full z-0 pointer-events-none">
+        <ParticlesBg />
+      </div>
+
+      <section className="w-11/12 mx-auto ">
+        <Banner />
+        <AboutMe></AboutMe>
+        <SkillsSection />
+        <Education />
+        <Projects />
+        <Contact />
+        <Footer />
+        <FaArrowCircleUp
+          onClick={bottomToTop}
+          className="fixed bottom-4 right-3 size-10 z-50 cursor-pointer"
+        />
+      </section>
+    </>
   );
 }
